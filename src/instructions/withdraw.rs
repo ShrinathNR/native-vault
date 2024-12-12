ea6a1384f1caadfcf54a5f6ddeadfee017271375
ShrinathNR <nr.shrinath@gmail.com>
2024-12-12 23:48:42 +0530

    add withdraw

--- src/instructions/withdraw.rs
+++ src/instructions/withdraw.rs
@@ -1,5 +1,24 @@
-use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult};
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, program::invoke_signed,
+    program_error::ProgramError, pubkey::Pubkey, system_instruction,
+};
 
-pub fn withdraw(_accounts: &[AccountInfo], _lamports: u64) -> ProgramResult {
-    todo!()
+pub fn withdraw(accounts: &[AccountInfo], lamports: u64) -> ProgramResult {
+    let [user, vault, _system_program] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    let vault_pda = Pubkey::find_program_address(&[b"vault", user.key.as_ref()], &crate::ID);
+
+    if vault_pda.0.ne(vault.key) {
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let transfer_ix = system_instruction::transfer(vault.key, user.key, lamports);
+
+    invoke_signed(
+        &transfer_ix,
+        &[user.clone(), vault.clone()],
+        &[&[b"vault", &user.key.to_bytes(), &[vault_pda.1]]],
+    )
 }
